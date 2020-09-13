a00fbbee7ae70d72fc9ee6c745300d8de37fa143
amin havasi <pau.ahq@gmail.com>
2020-09-13 17:53:07 +0430

    added new words

--- src/routes/rtest.js
+++ src/routes/rtest.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const Words = require('./../models/words');
+const { newWordValidator } = require('./../validator/rtestValidator');
 
-router.post('/newWord', async (req, res) => {});
+router.post('/newWord', async (req, res) => {
+    const { error } = await newWordValidator(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    try {
+        const word = await Words.findOne({ word: req.body.word });
+        if (word)
+            return res
+                .status(400)
+                .json({ error: 'This Word Already Exict!' })
+                .send();
+        const newWord = await new Words(req.body);
+        await newWord.save();
+        res.status(200).send('success saving');
+    } catch (err) {}
+});
 
 module.exports = router;
--- src/validator/rtestValidator.js
+++ src/validator/rtestValidator.js
@@ -1,10 +1,20 @@
 const Joi = require('joi');
-
+let pattern = /^[a-zA-Z][a-zA-Z]*[a-zA_Z]$/;
 const newWordValidator = (word) => {
     const newObject = Joi.object({
-        word: Joi.string().min(1).max(255).required(),
-        means: Joi.string().min(1).max(255).required(),
-        cat: Joi.string().min(1).max(255).required(),
+        word: Joi.string().min(1).max(255).required().regex(pattern),
+        means: Joi.string()
+            .min(1)
+            .max(255)
+            .required()
+            .regex(
+                /^[\u0622\u0627\u0628\u067E\u062A-\u062C\u0686\u062D-\u0632\u0698\u0633-\u063A\u0641\u0642\u06A9\u06AF\u0644-\u0648\u06CC]+$/
+            ),
+        cat: Joi.string().min(1).max(255).required().regex(pattern),
     });
     return newObject.validate(word);
+};
+
+module.exports = {
+    newWordValidator,
 };
